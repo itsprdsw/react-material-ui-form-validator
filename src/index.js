51cb7d7f46ffa7c74c1511df00cdecd5fd60d0e5
itsprdsw <its_prd_sw@gmv.com>
2018-04-10 16:21:58 +0200

    support for material-ui v1 beta 41

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { ValidatorComponent, ValidatorForm } from 'react-form-validator-core';
-
-import SelectValidator from './SelectValidator';
-import TextValidator from './TextValidator';
-
-exports.SelectValidator = SelectValidator;
-exports.TextValidator = TextValidator;
-exports.ValidatorComponent = ValidatorComponent;
-exports.ValidatorForm = ValidatorForm;
